ff50e7d27c41e11c1a1c121c1b94e4fcd6deb615
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-21 14:27:07 +0900

    게시글 가져오기, 수정 추가

--- src/main/java/com/samlsoft/demo/webservice/service/posts/PostsService.java
+++ src/main/java/com/samlsoft/demo/webservice/service/posts/PostsService.java
@@ -2,7 +2,9 @@ package com.samlsoft.demo.webservice.service.posts;
 
 import com.samlsoft.demo.webservice.domain.posts.Posts;
 import com.samlsoft.demo.webservice.domain.posts.PostsRepository;
+import com.samlsoft.demo.webservice.web.dto.PostsResponseDto;
 import com.samlsoft.demo.webservice.web.dto.PostsSaveRequestDto;
+import com.samlsoft.demo.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,18 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        PostsResponseDto postsResponseDto = new PostsResponseDto(posts);
+        return postsResponseDto;
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습ㄴ디ㅏ. id="+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+        return id;
     }
 }
