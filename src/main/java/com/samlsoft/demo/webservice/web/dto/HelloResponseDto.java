33c02abb2a4c7ef41b161d495664e84b5d7d6f3b
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-21 11:34:26 +0900

    유닛 테스트 작성 및 테스트 오류로 인한 gradle4로 버젼 다운

---
+++ src/main/java/com/samlsoft/demo/webservice/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.samlsoft.demo.webservice.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
