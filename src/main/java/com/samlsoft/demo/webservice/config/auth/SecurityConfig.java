16117fd127b58f4366b6f6e28bb96021c2d946a2
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-22 23:23:30 +0900

    스프링 시큐리티 설정추가

---
+++ src/main/java/com/samlsoft/demo/webservice/config/auth/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.samlsoft.demo.webservice.config.auth;
+
+import com.samlsoft.demo.webservice.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
