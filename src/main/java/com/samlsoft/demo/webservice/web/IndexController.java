2dd7acc7c37df0c684f6c97f611ac8fea9163818
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-22 23:08:06 +0900

    메인페이지 model에 유저추가

--- src/main/java/com/samlsoft/demo/webservice/web/IndexController.java
+++ src/main/java/com/samlsoft/demo/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.samlsoft.demo.webservice.web;
 
+import com.samlsoft.demo.webservice.config.auth.dto.SessionUser;
 import com.samlsoft.demo.webservice.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @Autowired
-    private PostsService postsService;
+    private final PostsService postsService;
+    @Autowired
+    private final HttpSession httpSession;
 
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -29,10 +38,7 @@ public class IndexController {
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
-        model.addAttribute("post",postsService.findById(id));
+        model.addAttribute("post", postsService.findById(id));
         return "posts-update";
     }
-
-
-
 }
