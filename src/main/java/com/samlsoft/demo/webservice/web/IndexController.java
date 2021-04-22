c57d89ac94d4e308ad4942fb0a3c659533b83a35
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-22 15:51:35 +0900

    게시판 컨트롤러
    인덱스, 수정화면, 삭제화면 매핑

---
+++ src/main/java/com/samlsoft/demo/webservice/web/IndexController.java
@@ -0,0 +1,38 @@
+package com.samlsoft.demo.webservice.web;
+
+import com.samlsoft.demo.webservice.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @Autowired
+    private PostsService postsService;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("post",postsService.findById(id));
+        return "posts-update";
+    }
+
+
+
+}
