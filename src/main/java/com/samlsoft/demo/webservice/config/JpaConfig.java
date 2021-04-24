7cee8908dba18b655280b133c40e5022c8b45f94
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-24 12:33:20 +0900

    모든 엔티티에 들어가는 시간필드를 작성하게 해주는 @EnableJpaAudting를 JpaConfig 추가

---
+++ src/main/java/com/samlsoft/demo/webservice/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.samlsoft.demo.webservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
