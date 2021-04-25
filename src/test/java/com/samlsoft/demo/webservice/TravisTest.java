31c52d429fd3de0241175a6d299c70a2746cffdb
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-25 12:45:44 +0900

    CI 관리용 travis 연동 테스트 파일

---
+++ src/test/java/com/samlsoft/demo/webservice/TravisTest.java
@@ -0,0 +1,12 @@
+package com.samlsoft.demo.webservice;
+
+import org.junit.Test;
+
+
+public class TravisTest {
+
+    @Test
+    public void Travis_연동테스트() {
+        System.out.println("Travis 연동 테스트");
+    }
+}
