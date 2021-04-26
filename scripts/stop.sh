9ceaac4a2fadb69b93984a91d9d2846ba38f83e5
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-27 00:03:32 +0900

    서버를 중지하는 쉘스크립트파일 추가

---
+++ scripts/stop.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSPATH}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> $IDLE_PORT starting application pid"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z $(IDLE_PID) ]
+then
+  echo "> do not stop current strating none application"
+else
+  echo "kill -15 $IDLE_PORT"
+  kill -15 ${IDLE_PORT}
+  sleep 5
+fi
