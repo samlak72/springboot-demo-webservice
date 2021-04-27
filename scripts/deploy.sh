cb24fcd6744f83368b4158526ecbac9593e38680
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-28 00:02:39 +0900

    AWS codedeploy 오류
    배포 오류로 확인하니 이전 배포 스크립트를 누락해서 deploy.sh파일 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=springboot-webservice
+
+echo "> Build file copy"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> current starting application pid"
+
+CURRENT_PID=$(pgreg -fl springboot-webservice | grep jar | awk '{print $1}')
+
+echo "> current string application pid : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> application is not current starting , do not end"
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> new application deploy"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> add execute role $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME"
+
+nohup java -jar \
+      -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-auth.yml,/home/ec2-user/app/application-real-db.yml
+      -Dspring.profile.active=real \
+      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
