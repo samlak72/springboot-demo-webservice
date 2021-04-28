ad995343328d6eb2c5116a5fdf12e337b67b5160
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-28 23:55:53 +0900

    오타 수정
    echo "> set..." -> echo "set ..."

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -10,7 +10,7 @@ function switch_proxy()
 
   echo "> Convert port: $IDLE_PORT"
   echo "> Conver port!"
-  echo "> set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
   echo "> nginx reload"
   sudo service nginx reload
 }
