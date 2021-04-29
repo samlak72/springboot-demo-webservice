c4b8a6edbd6401a7555503266cd5e33b032f8f46
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-04-29 15:37:26 +0900

    배포 오류 관련 수정
    port는 8082를 가져오고 profile은 real1으로 가져와서 이부분 테스트 출력 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,16 +4,19 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLE_PORT=$(find_idle_port)
+IDLE_PROFILE=$(find_idle_profile)
+echo "> idle profile: ${IDLE_PROFILE}"
 
+IDLE_PORT=$(find_idle_port)
 echo "> $IDLE_PORT starting application pid"
+
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
   echo "> do not stop current strating none application"
 else
-  echo "kill -15 IDLE_PID"
+  echo "kill -15 ${IDLE_PID}"
   kill -15 ${IDLE_PID}
   sleep 5
 fi
